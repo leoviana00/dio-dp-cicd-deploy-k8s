da2e536e0d31afd663267f7e8af78483ff66bd0b
Leonardo Vian Pereira <leonardoviana00@hotmail.com>
2024-10-11 13:40:38 -0300

    fix(k8s): test conectio jenkins k8s

--- infra/cicd/test-connection-k8s.groovy
+++ infra/cicd/test-connection-k8s.groovy
@@ -4,9 +4,9 @@ pipeline {
         stage('Test') {
             steps {
                 //Note: Replace below function with your generated pipeline script
-                withKubeConfig(caCertificate: '', clusterName: 'demo-cluster', contextName: 'demo-cluster', credentialsId: 'kube-jenkins', namespace: '', restrictKubeConfigAccess: false, serverUrl: 'https://10.0.0.10:6443') {
-                    sh "kubectl get ns"
-                    sh "helm ls"
+                withKubeConfig(caCertificate: '', clusterName: 'demo-cluster', contextName: 'demo-cluster', credentialsId: 'jenkins-account-k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: 'https://192.168.49.2:8443') {
+                    sh "kubectl get node"
+
                 }
             }
         }
