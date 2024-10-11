f2fd2eaf23c4050184b66772d5ae51c4d70354b5
Leonardo Vian Pereira <leonardoviana00@hotmail.com>
2024-10-11 14:16:07 -0300

    feat(front): ajuste url back

--- app/frontend/js.js
+++ app/frontend/js.js
@@ -5,7 +5,7 @@ $("#button-blue").on("click", function() {
     var txt_comentario = $("#comment").val();
 
     $.ajax({
-        url: "http://localhost:8000/",
+        url: "backend-php",
         
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
