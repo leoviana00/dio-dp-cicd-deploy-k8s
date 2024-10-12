aa453d2c4ade40baa44a85a7d4bc28d73402e2f8
Leonardo Vian Pereira <leonardoviana00@hotmail.com>
2024-10-11 23:21:41 -0300

    feat: frontend

--- app/application/js.js
+++ app/application/js.js
@@ -1,11 +1,11 @@
 $("#btn_gravar").on("click", function () {
   var txt_nome = $("#nome").val();
-  var txt_mensagem = $("#mensagem").val();
+  var txt_comentario = $("#comentario").val();
 
   $.ajax({
     url: "/incluir.php",
     type: "post",
-    data: { nome: txt_nome, mensagem: txt_mensagem },
+    data: { nome: txt_nome, comentario: txt_comentario },
     beforeSend: function () {
       $("#resposta").html("Enviando......");
     },
