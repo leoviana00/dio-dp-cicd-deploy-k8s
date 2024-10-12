a16586f2bfd72ea4b233bcb98d33ad360434ef37
Leonardo Vian Pereira <leonardoviana00@hotmail.com>
2024-10-11 23:11:16 -0300

    feat: configuracao banco de dados

--- app/application/conexao.php
+++ app/application/conexao.php
@@ -9,13 +9,6 @@ $database = "meubanco";
 
 $link = new mysqli($servername, $username, $password, $database);
 
-$sql = "CREATE TABLE IF NOT EXISTS mensagens (
-    id int,
-    nome varchar(50),
-    email varchar(50),
-    comentario varchar(100)
-)";
-
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
